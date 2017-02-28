01ec466ff2bb123c196e96539678d63d5a64653b
Joakim Lundin <joakim.lundin@bisnode.com>
2017-02-28 12:31:32 +0100

    Added ability to adjust smp, mem and docker port

--- qemu.sh
+++ qemu.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-qemu-system-aarch64 -cpu cortex-a57 -smp 4 -m 2048 -M virt -bios QEMU_EFI.fd -nographic \
+if [ -z "$SMP" ]; then
+  SMP=1
+fi
+
+if [ -z "$MEMORY_SIZE" ]; then
+  MEMORY_SIZE=1024
+fi
+
+if [ -z "$DOCKER_PORT" ]; then
+  DOCKER_PORT=2375
+fi
+
+qemu-system-aarch64 -cpu cortex-a57 -smp "${SMP}" -m "${MEMORY_SIZE}" -M virt -bios QEMU_EFI.fd -nographic \
   -device virtio-blk-device,drive=image -drive if=none,id=image,file=xenial-server-cloudimg-arm64-uefi1.img \
   -device virtio-blk-device,drive=cloud -drive if=none,id=cloud,file=cloud.img \
-  -netdev user,id=user0 -device virtio-net-device,netdev=user0 -redir tcp:2375::2375
+  -netdev user,id=user0 -device virtio-net-device,netdev=user0 -redir "tcp:${DOCKER_PORT}::2375"
